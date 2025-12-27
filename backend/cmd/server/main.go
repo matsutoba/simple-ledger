18242f89925b0e451f02dd9958eed608fb685511
matsutoba <matsubara_t@outlook.jp>
2025-12-27 15:34:01 +0900

    Userモデルの追加 - モデルの追加 - マイグレーションの追加 - シーダーの追加、パスワードのハッシュ関数を追加

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"simple-ledger/internal/common/config"
+	migration "simple-ledger/internal/common/db"
+	"simple-ledger/internal/common/db/seeder"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,6 +27,25 @@ func main() {
 		log.Fatalf("Failed to setup database: %v", err)
 	}
 	log.Printf("Database connected successfully: %v", db != nil)
+
+	/*
+	 * マイグレーション実行
+	 */
+	log.Print("Running database migrations...")
+	if err := migration.Migrate(db); err != nil {
+		log.Fatalf("failed to migrate: %v", err)
+	}
+	log.Print("Database migrations completed.")
+
+	/*
+	 * シードデータ投入
+	 */
+	env := config.GetEnv("APP_ENV", "development")
+	if env != "production" {
+		log.Print("Seeding initial data...")
+		seeder.SeedAll(db)
+		log.Print("Data seeding completed.")
+	}
 
 	// サーバー起動
 	if err := router.Run(":8080"); err != nil {
---
+++ backend/internal/common/db/migration.go
@@ -0,0 +1,14 @@
+package migration
+
+import (
+	"simple-ledger/internal/models"
+
+	"gorm.io/gorm"
+)
+
+func Migrate(db *gorm.DB) error {
+	if err := db.AutoMigrate(&models.User{}); err != nil {
+		return err
+	}
+	return nil
+}
