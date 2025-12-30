a58a4af2f4fb743b10c95409dd6a50e377005756
matsutoba <matsubara_t@outlook.jp>
2025-12-30 12:48:25 +0900

    lintエラーの修正

--- frontend/components/shadcn/ui/resizable.tsx
+++ frontend/components/shadcn/ui/resizable.tsx
@@ -1,9 +1,7 @@
-// @ts-nocheck
 'use client';
 
 import * as React from 'react';
 import { GripVerticalIcon } from 'lucide-react';
-// @ts-ignore
 import * as ResizablePrimitive from 'react-resizable-panels';
 
 import { cn } from './utils';
@@ -11,11 +9,10 @@ import { cn } from './utils';
 function ResizablePanelGroup({
   className,
   ...props
-}: React.ComponentProps<any>) {
+}: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <>
-      {/* @ts-ignore */}
-      <ResizablePrimitive.PanelGroup
+      <ResizablePrimitive.Group
         data-slot="resizable-panel-group"
         className={cn(
           'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
@@ -27,26 +24,16 @@ function ResizablePanelGroup({
   );
 }
 
-function ResizablePanel({ ...props }: React.ComponentProps<any>) {
-  return (
-    <>
-      {/* @ts-ignore */}
-      <ResizablePrimitive.Panel data-slot="resizable-panel" {...props} />
-    </>
-  );
-}
-
 function ResizableHandle({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<any> & {
+}: React.HTMLAttributes<HTMLDivElement> & {
   withHandle?: boolean;
 }) {
   return (
     <>
-      {/* @ts-ignore */}
-      <ResizablePrimitive.PanelResizeHandle
+      <ResizablePrimitive.Separator
         data-slot="resizable-handle"
         className={cn(
           'bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90',
@@ -59,9 +46,9 @@ function ResizableHandle({
             <GripVerticalIcon className="size-2.5" />
           </div>
         )}
-      </ResizablePrimitive.PanelResizeHandle>
+      </ResizablePrimitive.Separator>
     </>
   );
 }
 
-export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
+export { ResizablePanelGroup, ResizableHandle };
