a7cf14974e824ae1b82128f58bab30ce8fe90566
matsutoba <matsubara_t@outlook.jp>
2026-01-15 17:20:53 +0900

    取引追加ボタンを押したら取引追加モーダルを表示

--- frontend/components/features/transactions/Transactions.tsx
+++ frontend/components/features/transactions/Transactions.tsx
@@ -10,6 +10,7 @@ import { TransactionList } from './TransactionList';
 import { TransactionFilterBar } from './TransactionFilterBar';
 import { AmountCard } from '../common/AmountCard';
 import { Icon } from '@/components/ui/Icon';
+import { AddTransactionModal } from '../common/AddTransactionModal/AddTransactionModal';
 
 export const Tranasctions: React.FC = () => {
   const [transactions, setTransactions] =
@@ -17,28 +18,41 @@ export const Tranasctions: React.FC = () => {
   const [searchValue, setSearchValue] = useState('');
   const [categoryValue, setCategoryValue] =
     useState<TransactionFilterCategory>('all');
+  const [isOpenAddTransactionModal, setIsOpenAddTransactionModal] =
+    useState(false);
 
   return (
-    <BlockStack gap="lg">
-      <InlineStack alignItems="center" justifyContent="space-between">
-        <Typography variant="2xl">取引一覧</Typography>
-        <Button color="primary">
-          <Icon name="plus" />
-          取引を追加
-        </Button>
-      </InlineStack>
-      <TransactionFilterBar
-        searchValue={searchValue}
-        onSearchChange={setSearchValue}
-        categoryValue={categoryValue}
-        onCategoryChange={setCategoryValue}
+    <>
+      <BlockStack gap="lg">
+        <InlineStack alignItems="center" justifyContent="space-between">
+          <Typography variant="2xl">取引一覧</Typography>
+          <Button
+            color="primary"
+            onClick={() => setIsOpenAddTransactionModal(true)}
+          >
+            <Icon name="plus" />
+            取引を追加
+          </Button>
+        </InlineStack>
+        <TransactionFilterBar
+          searchValue={searchValue}
+          onSearchChange={setSearchValue}
+          categoryValue={categoryValue}
+          onCategoryChange={setCategoryValue}
+        />
+        <div className="w-full grid grid-cols-1 gap-2 grid-cols-3">
+          <AmountCard amount={750000} type="income" />
+          <AmountCard amount={38500} type="expense" />
+          <AmountCard amount={711500} type="balance" />
+        </div>
+        <TransactionList transactions={transactions} />
+      </BlockStack>
+
+      <AddTransactionModal
+        open={isOpenAddTransactionModal}
+        onClose={() => setIsOpenAddTransactionModal(false)}
+        onExecute={() => setIsOpenAddTransactionModal(false)}
       />
-      <div className="w-full grid grid-cols-1 gap-2 grid-cols-3">
-        <AmountCard amount={750000} type="income" />
-        <AmountCard amount={38500} type="expense" />
-        <AmountCard amount={711500} type="balance" />
-      </div>
-      <TransactionList transactions={transactions} />
-    </BlockStack>
+    </>
   );
 };
